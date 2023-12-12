370a303adbffe8b93cbe216c6a5c731a31b5f11c
Flame-eth <theflamepen@gmail.com>
2023-12-12 22:05:44 +0100

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
-import Image from "next/image";
+// import Font Awesome CSS
+import "@fortawesome/fontawesome-svg-core/styles.css"; 
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Tell Font Awesome to skip adding the CSS automatically 
+// since it's already imported above
+config.autoAddCss = false; 
 
 export default function Home() {
   return (
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+import React from "react";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="w-full py-5 px-7 flex justify-between bg-lightGreen items-center ">
+      <div className="flex gap-7 items-center">
+        <FontAwesomeIcon icon={faBars} className="text-[25px] text-white" />
+        <Link href="/">
+          <h1 className="">Dashboard</h1>
+        </Link>
+        <Link href="/contact">
+          <h1 className="">Contact</h1>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
