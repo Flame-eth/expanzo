c0136a761f9bb1aff48461e04e46db537bf0ab67
Flame-eth <theflamepen@gmail.com>
2023-12-12 22:45:49 +0100

    navbar done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,41 @@
-// import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { faBars } from "@fortawesome/free-solid-svg-icons/faBars";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Link from "next/link";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faBars, faPen } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 function Navbar() {
   return (
-    <div className="w-full py-5 px-7 flex justify-between bg-lightGreen items-center ">
+    <div className="w-full py-3 px-7 flex justify-between bg-lightGreen items-center ">
       <div className="flex gap-7 items-center">
-        <FontAwesomeIcon icon={faBars} className="text-[25px] text-white" />
+        <FontAwesomeIcon icon={faBars} className="text-[25px] text-[#87bfb2]" />
         <Link href="/">
-          <h1 className="">Dashboard</h1>
+          <h1 className="uppercase font-base font-bold text-white ">
+            Dashboard
+          </h1>
         </Link>
         <Link href="/contact">
-          <h1 className="">Contact</h1>
+          <h1 className="uppercase font-base font-bold text-white ">Contact</h1>
         </Link>
+      </div>
+      <div className="flex gap-7 items-center">
+        <div className="flex gap-3  items-center ">
+          <FontAwesomeIcon
+            icon={faUser}
+            className="text-[15px] text-[#87bfb2] border rounded-full p-2 bg-[#00c29d]"
+          />
+          <h1 className="uppercase font-base font-medium text-[#87bfb2] ">
+            Login
+          </h1>
+        </div>
+        <div className="flex gap-3  items-center ">
+          <FontAwesomeIcon
+            icon={faPen}
+            className="text-[15px] text-[#87bfb2] border rounded-full p-2 bg-[#00c29d]"
+          />
+          <h1 className="uppercase font-base font-medium text-[#87bfb2] ">
+            Registration
+          </h1>
+        </div>
       </div>
     </div>
   );
